ae4069d5d133c01e32ef385658ea21daf61e2819
flowerburied <2842511561@qq.com>
2023-12-20 21:39:01 +0800

    getway

---
+++ mall-getway/src/test/java/com/example/mall/getway/MallGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mall.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
