232209f41c6793ce9c8d944487e60ec833a99aec
flowerburied <2842511561@qq.com>
2023-12-21 12:21:44 +0800

    test gateway

--- mall-getway/src/main/java/com/example/mall/getway/MallGetwayApplication.java
+++ mall-getway/src/main/java/com/example/mall/getway/MallGetwayApplication.java
@@ -2,8 +2,11 @@ package com.example.mall.getway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MallGetwayApplication {
 
     public static void main(String[] args) {
