fe7d155ed314680ded408ea026a506361d1e1c9c
flowerburied <2842511561@qq.com>
2023-12-23 22:35:43 +0800

    Server direct signing oss version

--- mall-third-party/src/main/java/com/example/mall/thirdparty/controller/OssController.java
+++ mall-third-party/src/main/java/com/example/mall/thirdparty/controller/OssController.java
@@ -10,7 +10,9 @@ import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
 import com.aliyuncs.exceptions.ClientException;
+import com.example.common.utils.R;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,22 +23,21 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@RefreshScope
 @RestController
 public class OssController {
 
     @Resource
-    OSSClient ossClient;
+    OSS ossClient;
 
-    @Value("$spring.cloud.alicloud.access-key")
+    @Value("${spring.cloud.alicloud.access-key}")
     private String accessId;
 
     @RequestMapping("/oss/policy")
-    public Map<String, String> policy() throws ClientException {
-//        oss-cn-beijing.aliyuncs.com
-//        https://edu-test-202167.oss-cn-beijing.aliyuncs.com/001.png
-        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-//        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+    public R policy() throws ClientException {
 
+
+        System.out.println("accessId===" + accessId);
 
         // 填写Bucket名称，例如examplebucket。
         String bucket = "edu-test-202167";
@@ -51,6 +52,47 @@ public class OssController {
         String dir = format + "/";
         Map<String, String> respMap = null;
 
+//        try {
+//            long expireTime = 30;
+//            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+//            Date expiration = new Date(expireEndTime);
+//            // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。
+//            PolicyConditions policyConds = new PolicyConditions();
+//            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+//            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+//
+//            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+//            byte[] binaryData = postPolicy.getBytes("utf-8");
+////            String accessId = credentialsProvider.getCredentials().getAccessKeyId();
+//            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+//            String postSignature = ossClient.calculatePostSignature(postPolicy);
+//
+//            respMap = new LinkedHashMap<String, String>();
+//            respMap.put("accessid", accessId);
+//            respMap.put("policy", encodedPolicy);
+//            respMap.put("signature", postSignature);
+//            respMap.put("dir", dir);
+//            respMap.put("host", host);
+//            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+//            // respMap.put("expire", formatISO8601Date(expiration));
+//        } catch (Exception e) {
+//            // Assert.fail(e.getMessage());
+//            System.out.println(e.getMessage());
+//        } finally {
+//            ossClient.shutdown();
+//        }
+//
+//        return R.ok().put("data", respMap);
+
+
+
+
+//        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
+        // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
+        //String callbackUrl = "http://88.88.88.88:8888";
+//        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+//        String dir = format + "user-dir-prefix/"; // 用户上传文件时指定的前缀。
+//        Map<String, String> respMap = null;
         try {
             long expireTime = 30;
             long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
@@ -62,7 +104,6 @@ public class OssController {
 
             String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
             byte[] binaryData = postPolicy.getBytes("utf-8");
-//            String accessId = credentialsProvider.getCredentials().getAccessKeyId();
             String encodedPolicy = BinaryUtil.toBase64String(binaryData);
             String postSignature = ossClient.calculatePostSignature(postPolicy);
 
@@ -75,14 +116,11 @@ public class OssController {
             respMap.put("expire", String.valueOf(expireEndTime / 1000));
             // respMap.put("expire", formatISO8601Date(expiration));
 
-
         } catch (Exception e) {
-            // Assert.fail(e.getMessage());
             System.out.println(e.getMessage());
-        } finally {
-            ossClient.shutdown();
         }
+        return R.ok().put("data",respMap);
 
-        return respMap;
+
     }
 }
