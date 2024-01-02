df0d2398ea34bc6750d9fd31967113cebde9e5e1
flowerburied <2842511561@qq.com>
2024-01-02 21:44:55 +0800

    126 elasticSearch

---
+++ mall-search/src/main/java/com/example/mall/search/config/mallElasticSearchConfig.java
@@ -0,0 +1,21 @@
+package com.example.mall.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class mallElasticSearchConfig {
+
+    @Bean
+    public RestHighLevelClient esRestClient() {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+//                        new HttpHost("localhost", 9200, "http"),
+                        new HttpHost("192.168.18.128", 9201, "http")));
+        return client;
+    }
+
+}
