7be4536d0db2e420898bd4272f46f13f5e2317a3
flowerburied <2842511561@qq.com>
2024-01-08 18:43:03 +0800

    203 ready product detail

--- mall-search/src/main/java/com/example/mall/search/thread/ThreadTest.java
+++ mall-search/src/main/java/com/example/mall/search/thread/ThreadTest.java
@@ -11,6 +11,73 @@ public class ThreadTest {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         System.out.println("main start");
+//        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() -> {
+//            System.out.println("current thread" + Thread.currentThread().getId());
+//            int i = 10 / 2;
+//            System.out.println("运行结果" + i);
+//        }, executorService);
+//        CompletableFuture<Integer> integerCompletableFuture = CompletableFuture.supplyAsync(() -> {
+//            System.out.println("current thread" + Thread.currentThread().getId());
+//            int i = 10 / 2;
+//            System.out.println("运行结果" + i);
+//            return i;
+//        }, executorService).whenComplete((res, exception) -> {
+////            虽然能得到异常现象，但不能修改返回数据
+//            System.out.println("异步任务成功完成结果：" + res + "异常：" + exception);
+//        }).exceptionally(throwable -> {
+//            //出现异常默认返回
+//            return 10;
+//        });
+
+//        CompletableFuture<Integer> integerCompletableFuture = CompletableFuture.supplyAsync(() -> {
+//            System.out.println("current thread" + Thread.currentThread().getId());
+//            int i = 10 / 2;
+//            System.out.println("运行结果" + i);
+//            return i;
+//        }).handle((res, thr) -> {
+//            if (res != null) {
+//                return res * 2;
+//            }
+//            if (thr != null) {
+//                return 0;
+//            }
+//            return 0;
+//        });
+//        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+////            System.out.println("current thread" + Thread.currentThread().getId());
+////            int i = 10 / 2;
+////            System.out.println("运行结果" + i);
+////            return i;
+////        }, executorService).thenApplyAsync(res -> {
+////            System.out.println("运行2启动");
+////            return res + 1;
+////        });
+        CompletableFuture<String> future01 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("查询商品信息");
+            return "hello.jpg";
+        }, executorService);
+        CompletableFuture<String> future02 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("查询商品属性");
+            return "黑色256G";
+
+        }, executorService);
+        CompletableFuture<String> future03 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("查询商品介绍");
+            return "华为";
+        }, executorService);
+//        CompletableFuture<String> future03 = future01.thenCombineAsync(future02, (f1, f2) -> {
+//            System.out.println("运行3");
+//            return f1 + f2;
+//        }, executorService);
+//        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.anyOf(future01, future02, future03);
+//        voidCompletableFuture.get();  //等待所有结果完成·
+//        //阻塞获取
+//        Integer integer = integerCompletableFuture.get();
+        System.out.println("main end：" + future03.get());
+    }
+
+    public static void thread(String[] args) throws ExecutionException, InterruptedException {
+        System.out.println("main start");
         /**
          * 继承thread
          * 实现Runnable接口
